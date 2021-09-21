78514a5d351a512c8afcd107e6ad88be196df926
Joshua Ruvalcaba <Jruval253@west-mec.local>
2021-09-21 15:26:34 -0700

    i made header and navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {FiSearch} from 'react-icons/fi';
+import {IoNotificationsSharp}from 'react-icons/io5';
+
+const Header = () => {
+    return (
+        <div className = 'header'>
+            <div className="header__icon"></div>
+            
+            <div class="search__container">
+                <input class="search__input" type="text" placeholder="Search"/>
+                <FiSearch className = 'search__icon'/>
+            </div>
+
+            <div className="header__notification">
+                <IoNotificationsSharp className = 'bell'/>
+                <div className="red"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {FiSettings,FiMusic,FiHome,FiBookmark} from 'react-icons/fi';
+
+const NavBar = () => {
+    return (
+        <div className = 'navbar'>
+            <FiHome className = 'navbar__icon'/>
+            <FiMusic className = 'navbar__icon'/>
+            <FiBookmark className = 'navbar__icon'/>
+            <FiSettings className = 'navbar__icon'/>
+        </div>
+    )
+};
+
+export default NavBar;
