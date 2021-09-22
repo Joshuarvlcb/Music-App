8bdc611e1adc8f89475c7d1c33b91ee7592e23ef
Joshua Ruvalcaba <Jruval253@west-mec.local>
2021-09-22 15:27:58 -0700

    I am fetching the music now

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import {FiSettings,FiMusic,FiHome,FiBookmark} from 'react-icons/fi';
+import { useFetch } from '../util/Fetch';
+
 
 const NavBar = () => {
+    const {data} = useFetch();
+        console.log(data);
+
+
+
     return (
         <div className = 'navbar'>
             <FiHome className = 'navbar__icon'/>
---
+++ src/util/Fetch.js
@@ -0,0 +1,47 @@
+// import { useEffect } from 'react';
+
+// const API = `http://www.last.fm/api/auth/?api_key=da50ba867d4b993ab4ed997bb9b94a57`;
+
+
+//  export const  fetchMusic = () => {
+//      let mData
+//     const fetch = async () => {
+//         const response = await fetch(API);
+//         const data = await response.json();
+//        mData = data
+//     };
+
+//     useEffect(() => {
+//         fetch()
+//     }, [])
+
+//     return mData
+// };
+
+import { useEffect,useState } from 'react'
+const APIT_ENDPOINT = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=cher&api_key=da50ba867d4b993ab4ed997bb9b94a57&format=json'
+
+
+//searchQ will be something like &s=btaman
+export const useFetch = () => {
+    const [data, setData] = useState([])
+//url is always the api end point plus the search query
+const fetchMovie = async (url) => {
+
+    
+        try{
+            const response = await fetch(url);
+            const data = await response.json();
+            setData(data)
+          
+        }catch (error){
+            console.error(error)
+        }
+    }
+
+    useEffect( () => {
+    fetchMovie(`${APIT_ENDPOINT}`)
+    },[])
+
+    return {data};
+}
